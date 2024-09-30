b53674befcbed1060404dfb2d6ad4b19c849e1dd
Felipe Lima <felipinho.lima27@gmail.com>
2024-09-30 09:34:02 -0300

    teste

--- infra/rds/aws_rds.tf
+++ infra/rds/aws_rds.tf
@@ -48,6 +48,9 @@ resource "aws_db_instance" "banco" {
     publicly_accessible  = true
     port                 = 3306
     vpc_security_group_ids = [aws_security_group.banco_sg.id]
+    lifecycle {
+      ignore_changes = [snapshot_identifier]
+    }
     tags = {
       Name = "rdsDB"
     }
